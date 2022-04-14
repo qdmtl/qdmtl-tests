89646328d45d851be6118d072c4e0bf271530d41
davvalent <d@ntnlv.ca>
2022-04-14 18:42:31 -0400

    Py scripts pour générer graphes

---
+++ batch-convert-csv-ttl.py
@@ -0,0 +1,81 @@
+import csv, re, os
+
+# Paramètres
+inputFolder = '../qdmtl-data/CSV/'
+outputFolder = '../qdmtl-data/TTL/'
+filesWithBlankNodesAndNumberOfBN = {
+    'qdmtl-Record.csv' : 2
+}
+
+# liste de fichiers
+files = os.listdir(inputFolder)
+
+for file in files:
+    if file in filesWithBlankNodesAndNumberOfBN.keys():
+        continue
+    filesWithBlankNodesAndNumberOfBN[file] = 0
+
+for inputFile, number in filesWithBlankNodesAndNumberOfBN.items():
+    inputFilePath = inputFolder + inputFile
+    outputFile = re.match("(.+)\.csv", inputFile).group(1)
+    outputFile = outputFolder + outputFile + '-ABox.ttl'
+
+    # Initialisations
+    extractedData = []
+    propWithBlankNodeValue = {}
+    propWithBlankNode = []
+    numberOfPropWithBlankNode = number
+
+    # Chargement des données
+    with open(inputFilePath, 'r+', newline = '') as csvFile:
+        extractedData = [
+            dict(row) for row in csv.DictReader(csvFile, delimiter = ',')
+        ]
+
+    l = len(extractedData[0])
+
+    # Identification des propriétés avec noeuds vides
+    for i, key in enumerate(extractedData[0].keys()):
+        if i > l - numberOfPropWithBlankNode - 1:
+            propWithBlankNode.append(key)
+
+    # Écriture du fichier Turtle
+    with open(outputFile, 'w') as ABox:
+        for subject in extractedData:
+            for i, (property, value) in enumerate(subject.items()):
+
+                if value == '':
+                    continue
+
+                # si blank node, les props sont traitées en dehors de la boucle
+                if 1 < i < len(subject) - numberOfPropWithBlankNode:
+                    assertions += ' ;\n'
+
+                # voir structure switch Python 3.10
+                if property == 'URI':
+                    assertions = value + ' rdf:type owl:NamedIndividual ,\n'
+                elif property == 'rdf:type':
+                    assertions += '\t\t' + subject['rdf:type']
+                elif property in propWithBlankNode :
+                    propWithBlankNodeValue[property] = value
+                else:
+                    assertions += '\t' + property + ' ' + value
+
+            if propWithBlankNodeValue:
+
+                assertions += ' ;\n\trico:hasOrHadIdentifier '
+
+                for i, (property, value) in enumerate(propWithBlankNodeValue.items()):
+                    assertions += '[\n'
+                    assertions += '\t\trdf:type rico:Identifier ;\n'
+                    assertions += '\t\trico:hasIdentifierType ' + property + ' ;\n'
+                    assertions += '\t\trico:textualValue ' + value + '\n\t]'
+
+                    if i < len(propWithBlankNodeValue) - 1:
+                        assertions += ' , '
+
+                else:
+                    propWithBlankNodeValue = {}
+
+            assertions += ' .\n\n'
+            ABox.write(assertions)
---
+++ generate-graph.py
@@ -0,0 +1,85 @@
+import shutil, os
+from owlready2 import *
+from rdflib import Graph # doit être chargé après owlready2
+
+# configuration des chemins
+dataRootDir     = "../qdmtl-data/"  # répertoire des données (racine), relatif au script
+TBox            = "qdmtl-TBox.ttl"  # nom de fichier de la TBox
+ABoxDir         = "TTL/"            # répertoire de la ABox
+inferenceDir    = "TTL/inference/"  # répertoire des fichiers pour inférence
+tmpFile         = "tmp.ttl"         # fichier temporaire (TMP)
+inferenceTmpXML = "inf-tmp.rdf.xml" # nom de fichier inférence (TMP)
+ntGraphTmp      = "nt-graph-tmp.nt" #
+graph           = "qdmtl-graph.ttl" # nom de fichier du graphe (output)
+
+# construction des chemins
+TBox = dataRootDir + TBox
+ABoxDir = dataRootDir + ABoxDir
+graph = dataRootDir + graph
+inferenceDir = dataRootDir + inferenceDir
+tmpFile = dataRootDir + tmpFile
+inferenceTmpXML = dataRootDir + inferenceTmpXML
+ntGraphTmp = dataRootDir + ntGraphTmp
+
+# liste des graphes pour Pellet (inférence)
+inputReasoner = [file for file in os.listdir(inferenceDir)]
+
+# concat pour inference
+# print("concat pour inference")
+with open(tmpFile,'w') as tmpf: # tmp file
+
+    with open(TBox,'r') as input:
+        shutil.copyfileobj(input, tmpf)
+
+    for file in inputReasoner:
+        with open(inferenceDir + file,'r') as input:
+            shutil.copyfileobj(input, tmpf)
+
+# conversion
+tmpg = Graph()
+tmpg.parse(tmpFile) # turtle
+tmpg.serialize(destination = inferenceTmpXML, format = "xml") # xml
+
+# inférence
+onto_path.append(dataRootDir)
+qdmtl = get_ontology("file://" + inferenceTmpXML).load()
+with qdmtl:
+    sync_reasoner_pellet()
+
+qdmtl.save(file = ntGraphTmp, format = "ntriples")
+print("\n" + "onto_path: " + str(onto_path) + "\n")
+
+# récupérer préfixes
+with open(TBox,'r') as f:
+    prefixes = re.findall('(@prefix.+)((?:\n.+)+)(\.\n\n)', f.read(2000)) # 2000 char lus
+prefixes = "".join([i for i in prefixes[0]])
+
+# concat le reste des données
+# réutiliser ficher tmp (ici écrasé)
+with open(tmpFile,'w') as tmpgf:
+    tmpgf.write(prefixes)
+
+    for file in os.listdir(ABoxDir):
+        if os.path.isdir(ABoxDir + file):
+            continue
+        with open(ABoxDir + file,'r') as input: # ABox file
+            shutil.copyfileobj(input, tmpgf)
+
+# générer le graphe final
+qdmtlGraph = Graph()
+qdmtlGraph.parse(tmpFile) # turtle
+qdmtlGraph.parse(ntGraphTmp) # nt
+
+print("Saving qdmtl-graph")
+print("Please wait")
+qdmtlGraph.serialize(
+    destination = graph,
+    base = "http://ontology.qdmtl.ca/",
+    format = "ttl"
+)
+print("Done")
+
+# todo: nettoyage du répertoire
+# makefile
+# vérifier possibilité de manipuler plus avant les objets plutôt que création de fichier
+# par contre on libère la mémoire au fur et à mesure, c'est bien
